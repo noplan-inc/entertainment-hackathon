9e9ef3de7126e91d8a37453324d5b3155a8b2958
somasekimoto <0421.soma@gmail.com>
2023-04-02 09:33:24 +0900

    create: nft

--- packages/svg-nft-sandbox/scripts/deploy.js
+++ packages/svg-nft-sandbox/scripts/deploy.js
@@ -4,24 +4,18 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+// const hre = require("hardhat");
+// const { ethers } = require("hardhat");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+async function main () {
+  const DynamicImageNFT = await ethers.getContractFactory("DynamicImageNFT");
+  const dynamicImageNFT = await DynamicImageNFT.deploy();
+  await dynamicImageNFT.deployed();
+  console.log("DynamicImageNFT deployed to:", dynamicImageNFT.address);
+  return dynamicImageNFT;
 }
 
 // We recommend this pattern to be able to use async/await everywhere
