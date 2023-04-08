b699cd8593b98c3546a53a5f51aee2150483b233
somasekimoto <0421.soma@gmail.com>
2023-04-08 19:49:45 +0900

    create: words picking indices

--- packages/svg-nft-sandbox/contracts/Nonce.sol
+++ packages/svg-nft-sandbox/contracts/Nonce.sol
@@ -1,11 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 
-contract Nonce {
+
+contract Nonce is Ownable {
+
+    mapping (uint256 => uint256) public nonces;
     constructor() {}
 
-    function getNonce() public view returns (uint256) {
-        return block.prevrandao;
+    function setNonce(uint256 _round) public onlyOwner {
+        nonces[_round] = block.prevrandao;
+    }
+
+    function getNonce(uint256 _round) public view returns (uint256) {
+        return nonces[_round];
     }
 }
--- packages/svg-nft-sandbox/test/Nonce.js
+++ packages/svg-nft-sandbox/test/Nonce.js
@@ -2,9 +2,9 @@ const {
     time,
     loadFixture,
   } = require("@nomicfoundation/hardhat-network-helpers");
-  const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
   const { expect } = require("chai");
-const { BigNumber } = require("ethers");
+const { BigNumber, Contract } = require("ethers");
+const { ethers, helpers } = require("hardhat");
 
   describe("Nonce", function () {
     const words = ["shoos",
@@ -30,21 +30,46 @@ const { BigNumber } = require("ethers");
     }
 
     describe("createNonceMappings", function () {
-        it("Should return the right nonce array", async function () {
-            let todayWords = [];
-            const { nonce, owner } = await loadFixture(deployNonceFixture);
-            const _nonce = await nonce.getNonce();
-            console.log("_nonce");
-            console.log(_nonce);
-            // 1日のゲーム回数？
-            let gameCount = 6;
-            let wordsLength = BigNumber.from(words.length - 1);
-            for(let i = 0; i < gameCount; i++) {
-                let index = _nonce.mod(wordsLength).toNumber();
-                todayWords.push(words[index]);
+        beforeEach(async function () {
+            const {nonce, owner, otherAccount} = await loadFixture(deployNonceFixture);
+            this.nonce = nonce;
+            this.owner = owner;
+            this.otherAccount = otherAccount;
+        });
+        it("Should create the right nonces", async function () {
+            let nonces = [];
+            for (let i = 1; i < 11; i++) {
+                // 何回めのゲームか
+                let round = i
+                // そのゲームのnonceをセット
+                await this.nonce.setNonce(round);
+                // そのゲームのnonceを取得
+                _nonce = await this.nonce.getNonce(round);
+                // nonceを配列に入れる
+                nonces.push(_nonce);
+                // 時間進める
+                ethers.provider.send("evm_increaseTime", [384]); // 約384秒(1エポック)以上にする必要ある 12秒(ブロック生成時間) * 32ブロック
             }
-            // これだと全部同じ単語になる
-            console.log(todayWords);
+            expect(new Set(nonces).size === nonces.length).to.equal(true);
+        });
+        it("Should create the right indices", async function () {
+            let todayWords = [];
+            
+            let wordleSize = BigNumber.from(words.length);
+            // ゲームが10回あるとする
+            for (let i = 1; i < 11; i++) {
+                // 何回めのゲームか
+                let round = i
+                // そのゲームのnonceをセット
+                await this.nonce.setNonce(round);
+                // そのゲームのnonceを取得
+                _nonce = await this.nonce.getNonce(round);
+                // そのゲームのnonceをwordsのindexに変換
+                todayWords.push(words[_nonce % wordleSize]);
+                // 時間進める
+                ethers.provider.send("evm_increaseTime", [384]); // 約384秒(1エポック)以上にする必要ある 12秒(ブロック生成時間) * 32ブロック
+            }
+            expect(todayWords.length).to.equal(10);
         });
     });
 
