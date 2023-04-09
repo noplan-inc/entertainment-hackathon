9e255fd98b0c64ebb7ccfadccdefe3040132b461
serinuntius <wasab1gj@gmail.com>
2023-04-09 23:02:31 +0900

    update

--- packages/svg-nft-sandbox/contracts/ZKWordle.sol
+++ packages/svg-nft-sandbox/contracts/ZKWordle.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "./AnswerVerifier.sol";
 
 
-
-contract ZKWordle is Ownable {
+contract ZKWordle is Ownable, AnswerVerifier {
     using Counters for Counters.Counter;
 
     Counters.Counter public round;
@@ -24,5 +24,31 @@ contract ZKWordle is Ownable {
     // RANDAOの値を返すview関数
     function getRandao() public view returns (uint256) {
         return block.prevrandao; // block.difficulty
+    }
+
+    function bytes32ToUintArray(bytes32 data) public pure returns (uint[8] memory) {
+        uint[8] memory result;
+
+        // Split the bytes32 into chunks of 32 bits
+        for (uint i = 0; i < 8; i++) {
+            uint chunk = uint256(data << (32 * i)) >> 224;
+            result[i] = chunk;
+        }
+
+        return result;
+    }
+
+    function getAnswerHash() public view returns (bytes32) {
+        uint256 _round = round.current();
+        return questions[_round];
+    }
+
+    function answer(Proof memory proof) public {
+        bytes32 hash = getAnswerHash();
+        // TOOD: すでに回答された問題は回答できない
+
+        uint[8] memory _answer = bytes32ToUintArray(hash);
+
+        require(super.verifyAnswer(proof, _answer), "Answer is wrong");
     }
 }
--- packages/svg-nft-sandbox/test/ZKWordle.js
+++ packages/svg-nft-sandbox/test/ZKWordle.js
@@ -5,6 +5,21 @@ const {
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 const {wodles} = require("./wordleMaster");
+const {getWordDec, getWordHex, addressToUintArray} = require("./utils");
+const fs = require('fs');
+
+const zkSource = `
+import "hashes/sha256/sha256Padded";
+
+def main(private u8[5] word, u32[8] expectedHash,private  u32[8] addressUint, u32[8] pubAddressUint) -> bool {
+    u32[8] hash = sha256Padded(word);
+
+    assert(hash == expectedHash);
+    assert(addressUint == pubAddressUint);
+    return true;
+}
+`;
+
 
 describe("ZKWordle", function () {
   // We define a fixture to reuse the same setup in every test.
@@ -54,6 +69,50 @@ describe("ZKWordle", function () {
 
       expect(await zkWordle.round()).to.equal(1);
       
+    });
+  });
+
+  describe("answer", function() {
+    it("should success", async function() {
+      let { initialize } = await import("zokrates-js");
+
+      const zokrates = (await initialize()).withOptions({
+        curve: "bn128",
+        scheme: "g16",
+        backend: "ark"
+      });
+      const { zkWordle, otherAccount } = await loadFixture(deployFixture);
+
+      const size = wodles.length;
+      const randao = await zkWordle.getRandao();
+      const index = randao.mod(size);
+      const _answer = wodles[index];
+      const _hashedAnswer = ethers.utils.sha256(ethers.utils.toUtf8Bytes(_answer));
+
+      await expect(zkWordle.createQuestion(
+        _hashedAnswer
+      )).not.to.be.reverted;
+
+      const artifacts = zokrates.compile(zkSource);
+
+      const answer = getWordDec(_answer);
+      const hashedAnswer = getWordHex(_answer);
+      const address = addressToUintArray(otherAccount.address);
+
+      console.log(`word: ${answer}, type: ${typeof answer}`);
+      console.log(`expectedHash(dec chunked): ${hashedAnswer}, type: ${typeof hashedAnswer}`);
+      console.log(`address: ${address}, type: ${typeof address}`);
+
+      const {witness, output} = zokrates.computeWitness(artifacts, [answer, hashedAnswer, address, address]);
+
+      const key = fs.readFileSync('./test/proving.key');
+
+      const {proof}=  zokrates.generateProof(artifacts.program,witness, key);
+      console.log(`proof: ${proof}, type: ${typeof proof}, \n ${JSON.stringify(proof)}}`);
+      // console.log(JSON.stringify(keypair));
+      const {a, b, c} = proof;
+
+      await expect(zkWordle.answer([a,b,c])).not.to.be.reverted;
     });
   });
 });
