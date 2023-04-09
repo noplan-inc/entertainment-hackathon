0f4f5612b6095e3f790d55f65c083833233878f4
serinuntius <wasab1gj@gmail.com>
2023-04-09 18:31:10 +0900

    updaste

--- packages/zkp/answer/sha256.js
+++ packages/zkp/answer/sha256.js
@@ -1,5 +1,7 @@
 const args = process.argv.slice(2)[0];
 
+console.log(`word: ${[...args].map(c => c.charCodeAt(0)).join(' ')}`);
+
 function splitByChunk(str, size) {
   const numChunks = Math.ceil(str.length / size)
   const chunks = new Array(numChunks)
@@ -12,11 +14,7 @@ function splitByChunk(str, size) {
 const crypto = require('crypto');
 const hex = crypto.createHash('sha256').update(Buffer.from(args)).digest('hex');
 const chunked = splitByChunk(hex, 8).map(e => `0x${e}`)
-const str = chunked.join(',');
-console.log(`hex digest: [${str}]`);
-
-
 // hexをdecに変換してconsole.log
-console.log(`dec chunked: ${chunked.map(e => parseInt(e, 16)).join(' ')}`);
+console.log(`expectedHash(dec chunked): ${chunked.map(e => parseInt(e, 16)).join(' ')}`);
 
 
